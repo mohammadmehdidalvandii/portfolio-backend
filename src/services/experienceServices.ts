a33267cc3d591098062795ccbc437ea041a6f141
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-06 15:00:18 +0330

    created experienceServerices

---
+++ src/services/experienceServices.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+import ExperienceModel from "../models/experience";
+import { ExperienceProps } from "../types/experience";
+
+export const experienceServices = {
+    async createExperience(data:ExperienceProps){
+        const {jobTitle , company , period , description} = data;
+        if(!jobTitle || !company || !period || !description){
+            throw new Error('All felids are required')
+        };
+
+        const newExperience = await ExperienceModel.create({
+            jobTitle,
+            company,
+            period,
+            description,
+        });
+
+        return newExperience
+    },
+    async getExperience(){
+        const experiences = await ExperienceModel.find({});
+        return experiences
+    },
+    async updateExperience(id:string,data:Partial<ExperienceProps>){
+        if(!mongoose.Types.ObjectId.isValid(id)){
+            throw new Error("Invalid experience ID");
+        }
+        const experience = await ExperienceModel.findOneAndUpdate(
+            {_id:id},
+            data,
+            {new:true}
+        )
+        return experience
+    }
+}
