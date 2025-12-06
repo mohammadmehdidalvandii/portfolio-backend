fd69bfee5e8d129417f3eedabc2e0d4ec9291dad
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2025-12-06 15:00:41 +0330

    created experienceController

---
+++ src/routes/experienceRoutes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { experienceController } from "../controller/experience.controller";
+
+const router = Router();
+
+router.get('/' , experienceController.get);
+router.post('/create', experienceController.create);
+router.patch('/update/:id', experienceController.update);
+
+export default router
